cfb40b8305c6fc9dbdebb04ce116b2112fc5774e
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-06 22:40:52 +0200

    feat: implementa AppDbContext per indicare ad EntityFramework quali sono le classi corrispondenti a tabelle del database

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebForm1App.models;
+
+namespace WebForm1App
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext() : base("name=db_locale")
+        {
+
+        }
+
+        public DbSet<Prodotto> Prodotti { get; set; }
+    }
+}
