967da9f4557d411837fa23d77e1507c374310a51
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-06 22:43:12 +0200

    feat: implementa migration per la creazione della tabella corrispondente alla Entity Prodotto

---
+++ Migrations/202604061956075_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace WebForm1App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Prodottoes",
+                c => new
+                    {
+                        IdProdotto = c.Int(nullable: false, identity: true),
+                        Titolo = c.String(),
+                        Prezzo = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdProdotto);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Prodottoes");
+        }
+    }
+}
