4f2ecf8d2e5692f6c460c78f4d26167cc39f2397
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-06 22:38:43 +0200

    feat: implementa Entity Prodotto

---
+++ models/Prodotto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebForm1App.models
+{
+    public class Prodotto
+    {
+        [Key]
+        public int IdProdotto { get; set; }
+        public string Titolo { get; set; }
+        public double Prezzo { get; set; }
+    }
+}
