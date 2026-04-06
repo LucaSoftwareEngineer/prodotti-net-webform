7c9cc0647c6528d20619cc710df72f98c9316c7d
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-06 22:38:11 +0200

    initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace WebForm1App
+{
+    public class BundleConfig
+    {
+        // Per altre informazioni sulla creazione di bundle, vedere https://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+        }
+    }
+}
