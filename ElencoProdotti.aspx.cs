5b8215b1390d8c9ab5c5debd32f1e7fd80d34493
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-06 22:44:26 +0200

    feat: implementa Pagina e Classe che mostra l'elenco dei prodotti salvati nel database

---
+++ ElencoProdotti.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm1App
+{
+    public partial class ElencoProdottiaspx : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var db = new AppDbContext()) {
+                var elenco = db.Prodotti.OrderBy(p => p.Titolo).ToList();
+                GridViewProdotti.DataSource = elenco;
+                GridViewProdotti.DataBind();
+            }
+        }
+    }
+}
