fb3b6498e681ae7a1157d4ddfcd185a7c1f7a0ad
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-10 07:00:51 +0200

    feat: aggiunge Migration relativa alla tabella dbo.managers, entity Manager

---
+++ Migrations/202604100443265_ManagerTable.cs
@@ -0,0 +1,27 @@
+﻿namespace WebForm1App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ManagerTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.managers",
+                c => new
+                    {
+                        man_id = c.Int(nullable: false, identity: true),
+                        man_username = c.String(),
+                        man_password = c.String(),
+                    })
+                .PrimaryKey(t => t.man_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.managers");
+        }
+    }
+}
